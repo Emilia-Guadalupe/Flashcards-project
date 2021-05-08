0bd88d00bf9f5fbee2d88fa346b61c5205906830
Emilia <m.emilia.guadalupe@gmail.com>
2021-05-08 13:32:00 -0300

    Components

--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -1,17 +1,22 @@
 import React from 'react';
-import './card.css';
+import './cardStyles.css';
 
 function Card(props){
     return(
         <div className="card-container">
             <div className="card">
                 <div className="front">
-                    <div className="eng"></div>
+                    <div className="eng">
+                        {props.eng}
+                    </div>
                 </div>
-                <div className="back"></div>
+                <div className="back">
+                    <div className="han">{props.han}</div>
+                    <div className="pin">{props.pin}</div>
+                </div>
             </div>
         </div>
     )
 }
 
-export default Card; 
+export default Card;
